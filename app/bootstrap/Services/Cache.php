d64ae1c3f2176b2ed4795fe23f8a05b7119d93d0
Dennis Stücken <dstuecken@me.com>
2023-10-06 16:33:07 -0700

    fixing redis index configuration

--- app/bootstrap/Services/Cache.php
+++ app/bootstrap/Services/Cache.php
@@ -49,11 +49,11 @@ return function (\DS\Interfaces\GeneralApplication $application, Phalcon\Di\Fact
             return new \DS\Component\Cache\Redis(
                 [
                     "lifetime" => \DS\Model\Helper\Seconds::DaysOne,
-                    "prefix" => $config['redis']['prefix'] ?: "fw.cache." . APP_VERSION . ".",
-                    'host' => $config['redis']['host'] ?: 'localhost',
+                    "prefix" => $config['redis']['prefix'] ?? "fw.cache." . APP_VERSION . ".",
+                    'host' => $config['redis']['host'] ?? 'localhost',
                     'port' => $config['redis']['port'],
-                    'index' => $config['redis']['index'] ?: null,
-                    'auth' => $config['redis']['auth'] ?: null,
+                    'index' => $config['redis']['index'] ?? null,
+                    'auth' => $config['redis']['auth'] ?? null,
                     "persistent" => true,
                 ]
             );
@@ -103,12 +103,12 @@ return function (\DS\Interfaces\GeneralApplication $application, Phalcon\Di\Fact
             return new \Phalcon\Mvc\Model\MetaData\Redis(
                 $adapterFactory,
                 [
-                    'host' => $config['redis']['host'] ?: 'localhost',
+                    'host' => $config['redis']['host'] ?? 'localhost',
                     'port' => $config['redis']['port'],
-                    'auth' => $config['redis']['auth'] ?: null,
+                    'auth' => $config['redis']['auth'] ?? null,
                     "lifetime" => \DS\Model\Helper\Seconds::WeeksOne,
-                    "prefix" => $config['redis']['prefix'] ?: "fw." . APP_VERSION . ".metadata.",
-                    'index' => 1,
+                    "prefix" => $config['redis']['prefix'] ?? "fw." . APP_VERSION . ".metadata.",
+                    'index' => $config['redis']['index'] ?? null,
                 ]
             );
         }
