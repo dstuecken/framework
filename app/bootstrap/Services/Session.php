5f51198351017c79b7ec7b6f98afe21c645f36ca
Dennis Stücken <dstuecken@me.com>
2024-05-09 17:29:25 -0700

    Add session defaults and dynamic adapter handling
    This commit introduces session configuration defaults and enhances dynamic session adapter management. In addition to the hardcoded values replaced with defaults, it also supports session handling where the Redis adapter is used if available; otherwise, it falls back to file-based sessions.

--- app/bootstrap/Services/Session.php
+++ app/bootstrap/Services/Session.php
@@ -1,5 +1,30 @@
 <?php
 
+if (!defined('FRAMEWORK_DEFAULT_SESSION_LIFETIME'))
+{
+    define('FRAMEWORK_DEFAULT_SESSION_LIFETIME', 2592000);
+}
+
+if (!defined('FRAMEWORK_DEFAULT_SESSION_PREFIX'))
+{
+    define('FRAMEWORK_DEFAULT_SESSION_PREFIX', 'php.session.');
+}
+
+if (!defined('FRAMEWORK_DEFAULT_SESSION_PERSISTENT'))
+{
+    define('FRAMEWORK_DEFAULT_SESSION_PERSISTENT', true);
+}
+
+if (!defined('FRAMEWORK_DEFAULT_SESSION_SERIALIZER'))
+{
+    define('FRAMEWORK_DEFAULT_SESSION_SERIALIZER', 'Json');
+}
+
+if (!defined('FRAMEWORK_DEFAULT_SESSION_SAVE_PATH'))
+{
+    define('FRAMEWORK_DEFAULT_SESSION_SAVE_PATH', '/tmp');
+}
+
 /**
  * DS-Framework
  *
@@ -8,57 +33,48 @@
  * @package DS
  * @version $Version$
  */
-return function (\DS\Interfaces\GeneralApplication $application, Phalcon\Di\FactoryDefault $di) {
-    
-    /*
-    $di->setShared(
-        'session',
-        function () use ($di, $application) {
-            $session = new \DS\Component\Session\Manager();
-            $files   = new Stream(
-                [
-                    'savePath' => '/tmp',
-                ]
-            );
-            $session->setAdapter($files);
-            $session->start();
-            
-            return $session;
-        }
-    );
-*/
-    
+return function (\DS\Interfaces\GeneralApplication $application, Phalcon\Di\FactoryDefault $di)
+{
+
     // Register 'session'
     $di->setShared(
         'session',
-        function () use ($di, $application) {
+        function () use ($di, $application)
+        {
+            $config  = $application->getConfig();
             $session = new \DS\Component\Session\Manager();
-            
-            /*
-            *     'prefix' => 'sess-redis-',
-            *     'host' => '127.0.0.1',
-            *     'port' => 6379,
-            *     'index' => 0,
-            *     'persistent' => false,
-            *     'auth' => '',
-            *     'socket' => ''
-            */
-            $config       = $application->getConfig();
-            $redisAdapter = new \DS\Component\Session\Adapter\RedisAdapter(
-                new \Phalcon\Storage\AdapterFactory(new \Phalcon\Storage\SerializerFactory),
-                [
-                    'prefix' => 'fw.session.',
-                    'host' => $config['redis']->host ?? 'localhost',
-                    'port' => $config['redis']->port ?? 6379,
-                    'auth' => $config['redis']->auth ?? null,
-                    'persistent' => true,
-                    'index' => $config['redis']->index ?? 1,
-                ]
-            );
-            
-            $session->setAdapter($redisAdapter);
+
+            if ($config['redis'])
+            {
+                $redisAdapter = new \DS\Component\Session\Adapter\RedisAdapter(
+                    new \Phalcon\Storage\AdapterFactory(new \Phalcon\Storage\SerializerFactory),
+                    [
+                        'prefix' => $config['session']->prefix ?? FRAMEWORK_DEFAULT_SESSION_PREFIX,
+                        'host' => $config['redis']->host ?? 'localhost',
+                        'port' => $config['redis']->port ?? 6379,
+                        'auth' => $config['redis']->auth ?? null,
+                        'persistent' => FRAMEWORK_DEFAULT_SESSION_PERSISTENT,
+                        'index' => $config['redis']->index ?? 1,
+                        'defaultSerializer' => $config['session']->prefix ?? FRAMEWORK_DEFAULT_SESSION_SERIALIZER,
+                        'lifetime' => $config['session']->prefix ?? 'php.session.' ?? FRAMEWORK_DEFAULT_SESSION_LIFETIME,
+                    ]
+                );
+
+                $session->setAdapter($redisAdapter);
+            }
+            else
+            {
+                $files = new \Phalcon\Session\Adapter\Stream(
+                    [
+                        'savePath' => FRAMEWORK_DEFAULT_SESSION_SAVE_PATH,
+                    ]
+                );
+                $session->setAdapter($files);
+            }
+
+
             $session->start();
-            
+
             return $session;
         }
     );
